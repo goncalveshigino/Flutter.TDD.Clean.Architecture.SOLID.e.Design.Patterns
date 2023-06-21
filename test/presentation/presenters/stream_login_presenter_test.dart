d5dcec32fc6044041fc6ca1ebe5f24f84aea1fc5
goncalveshigino <goncalveshigino20@gmail.com>
2023-06-21 22:16:55 +0100

    chore:Notificar o emailErrorStream com o mesmo erro do validation, caso retorne erro

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,31 +9,64 @@ abstract class Validation {
   String validate({@required String field, @required String value});
 }
 
+class LoginState {
+  String emailError;
+}
+
 class StreamLoginPresenter {
+
   final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+
+  var _state = LoginState();
+
+  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError );
 
   StreamLoginPresenter({@required this.validation});
 
   void validateEmail(String email) {
-    validation.validate(field: 'email', value: email);
+   _state.emailError = validation.validate(field: 'email', value: email);
+   _controller.add(_state);
   }
 }
 
 class ValidationSpy extends Mock implements Validation {}
 
 void main() {
+
   StreamLoginPresenter sut;
   ValidationSpy validation;
   String email;
+
+  PostExpectation mockValidationCall(String field) => 
+    when(validation.validate(field: field == null ? anyNamed('field') : field, value: anyNamed('value')));
+
+  void mockValidation({String field, String value}) {
+    mockValidationCall(field).thenReturn(value);
+  }
 
   setUp(() {
      validation = ValidationSpy();
      sut = StreamLoginPresenter(validation: validation);
      email = faker.internet.email();
+     mockValidation();
   });
+
+  
+
+
   test('Should call Validation with correct email', () {
     sut.validateEmail(email);
 
     verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+//Testando uma Stream
+  test('Should call Validation with correct email', () {
+   mockValidation(value: 'error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
   });
 }
